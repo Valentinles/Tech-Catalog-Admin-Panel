f8a5579aac12ef66c1cb8daf4119d9ab53df8f31
Valentinles <valentinvn@abv.bg>
2019-12-30 10:47:02 +0200

    data service added

---
+++ src/TechCatalogAdminPanel.Services/Implementations/DataService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TechCatalogAdminPanel.Data;
+
+namespace TechCatalogAdminPanel.Services.Implementations
+{
+    public class DataService
+    {
+        protected readonly TechCatalogDbContext context;
+        public DataService(TechCatalogDbContext context)
+        {
+            this.context = context;
+        }
+    }
+}
