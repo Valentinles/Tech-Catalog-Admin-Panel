4fb6333709f406deddf3eb4d8cd8a7e93deac868
Valentinles <valentinvn@abv.bg>
2020-01-08 14:52:25 +0200

    revers maps added

--- src/TechCatalogAdminPanel.Common/Mapper/AutoMapperConfiguration.cs
+++ src/TechCatalogAdminPanel.Common/Mapper/AutoMapperConfiguration.cs
@@ -12,36 +12,36 @@ namespace TechCatalogAdminPanel.Common.Mapper
         public AutoMapperConfiguration()
         {
             //binding mappings
-            this.CreateMap<AirConditioner, AirConditionerBindingModel>();
-            this.CreateMap<Coocker, CoockerBindingModel>();
-            this.CreateMap<DesktopPc, DesktopPcBindingModel>();
-            this.CreateMap<GameConsole, GameConsoleBindingModel>();
-            this.CreateMap<Headphone, HeadphoneBindingModel>();
-            this.CreateMap<Keyboard, KeyboardBindingModel>();
-            this.CreateMap<Laptop, LaptopBindingModel>();
-            this.CreateMap<MobilePhone, MobilePhoneBindingModel>();
-            this.CreateMap<Monitor, MonitorBindingModel>();
-            this.CreateMap<Mouse, MouseBindingModel>();
-            this.CreateMap<Refrigerator, RefrigeratorBindingModel>();
-            this.CreateMap<Tablet, TabletBindingModel>();
-            this.CreateMap<Tv, TvBindingModel>();
-            this.CreateMap<WashingMachine, WashingMachineBindingModel>();
+            this.CreateMap<AirConditioner, AirConditionerBindingModel>().ReverseMap();
+            this.CreateMap<Coocker, CoockerBindingModel>().ReverseMap();
+            this.CreateMap<DesktopPc, DesktopPcBindingModel>().ReverseMap();
+            this.CreateMap<GameConsole, GameConsoleBindingModel>().ReverseMap();
+            this.CreateMap<Headphone, HeadphoneBindingModel>().ReverseMap();
+            this.CreateMap<Keyboard, KeyboardBindingModel>().ReverseMap();
+            this.CreateMap<Laptop, LaptopBindingModel>().ReverseMap();
+            this.CreateMap<MobilePhone, MobilePhoneBindingModel>().ReverseMap();
+            this.CreateMap<Monitor, MonitorBindingModel>().ReverseMap();
+            this.CreateMap<Mouse, MouseBindingModel>().ReverseMap();
+            this.CreateMap<Refrigerator, RefrigeratorBindingModel>().ReverseMap();
+            this.CreateMap<Tablet, TabletBindingModel>().ReverseMap();
+            this.CreateMap<Tv, TvBindingModel>().ReverseMap();
+            this.CreateMap<WashingMachine, WashingMachineBindingModel>().ReverseMap();
 
             //edit mappings
-            this.CreateMap<AirConditioner, AirConditionerEditViewModel>();
-            this.CreateMap<Coocker, CoockerEditViewModel>();
-            this.CreateMap<DesktopPc, DesktopPcEditViewModel>();
-            this.CreateMap<GameConsole, GameConsoleEditViewModel>();
-            this.CreateMap<Headphone, HeadphoneEditViewModel>();
-            this.CreateMap<Keyboard, KeyboardEditViewModel>();
-            this.CreateMap<Laptop, LaptopEditViewModel>();
-            this.CreateMap<MobilePhone, MobilePhoneEditViewModel>();
-            this.CreateMap<Monitor, MonitorEditViewModel>();
-            this.CreateMap<Mouse, MouseEditViewModel>();
-            this.CreateMap<Refrigerator, RefrigeratorEditViewModel>();
-            this.CreateMap<Tablet, TabletEditViewModel>();
-            this.CreateMap<Tv, TvEditViewModel>();
-            this.CreateMap<WashingMachine, WashingMachineEditViewModel>();
+            this.CreateMap<AirConditioner, AirConditionerEditViewModel>().ReverseMap();
+            this.CreateMap<Coocker, CoockerEditViewModel>().ReverseMap();
+            this.CreateMap<DesktopPc, DesktopPcEditViewModel>().ReverseMap();
+            this.CreateMap<GameConsole, GameConsoleEditViewModel>().ReverseMap();
+            this.CreateMap<Headphone, HeadphoneEditViewModel>().ReverseMap();
+            this.CreateMap<Keyboard, KeyboardEditViewModel>().ReverseMap();
+            this.CreateMap<Laptop, LaptopEditViewModel>().ReverseMap();
+            this.CreateMap<MobilePhone, MobilePhoneEditViewModel>().ReverseMap();
+            this.CreateMap<Monitor, MonitorEditViewModel>().ReverseMap();
+            this.CreateMap<Mouse, MouseEditViewModel>().ReverseMap();
+            this.CreateMap<Refrigerator, RefrigeratorEditViewModel>().ReverseMap();
+            this.CreateMap<Tablet, TabletEditViewModel>().ReverseMap();
+            this.CreateMap<Tv, TvEditViewModel>().ReverseMap();
+            this.CreateMap<WashingMachine, WashingMachineEditViewModel>().ReverseMap();
 
             //listing mappings
             this.CreateMap<AirConditioner, DeviceListingViewModel>();
