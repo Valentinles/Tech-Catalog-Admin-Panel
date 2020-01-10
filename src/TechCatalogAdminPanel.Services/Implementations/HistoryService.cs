5f5a3a148268b62282efdb48b2afcd7e1f0dc1c3
Valentinles <valentinvn@abv.bg>
2020-01-10 15:14:25 +0200

    history service and interface added

---
+++ src/TechCatalogAdminPanel.Services/Implementations/HistoryService.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechCatalogAdminPanel.Data;
+using TechCatalogAdminPanel.Models;
+using TechCatalogAdminPanel.Services.Interfaces;
+
+namespace TechCatalogAdminPanel.Services.Implementations
+{
+    public class HistoryService : DataService, IHistoryService
+    {
+        public readonly IMapper mapper;
+        public HistoryService(IMapper mapper, TechCatalogDbContext context) : base(context)
+        {
+            this.mapper = mapper;
+        }
+
+        public async Task<IEnumerable<History>> GetAllHistories()
+        {
+            var histories = await this.context.Histories.
+                OrderByDescending(h => h.Id)
+                .ToListAsync();
+
+            return histories;
+        }
+    }
+}
---
+++ src/TechCatalogAdminPanel.Services/Interfaces/IHistoryService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TechCatalogAdminPanel.Models;
+
+namespace TechCatalogAdminPanel.Services.Interfaces
+{
+    public interface IHistoryService
+    {
+        Task<IEnumerable<History>> GetAllHistories();
+    }
+}
