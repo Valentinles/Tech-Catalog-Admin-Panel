6446237fefc6d5b2a8ebf869ff296958cd10549c
Valentinles <valentinvn@abv.bg>
2020-01-10 15:07:07 +0200

    historyListingViewModel added

---
+++ src/TechCatalogAdminPanel.Common/ViewModels/HistoryListingViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TechCatalogAdminPanel.Common.ViewModels
+{
+    public class HistoryListingViewModel
+    {
+        public string Text { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+    }
+}
