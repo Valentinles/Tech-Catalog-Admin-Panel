c115f4d488a06ddf68de7632fe02f9424300bcb6
Valentinles <valentinvn@abv.bg>
2020-01-10 14:37:55 +0200

    History model added

---
+++ src/TechCatalogAdminPanel.Models/History.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TechCatalogAdminPanel.Models
+{
+    public class History
+    {
+        public int Id { get; set; }
+
+        public string Text { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+    }
+}
